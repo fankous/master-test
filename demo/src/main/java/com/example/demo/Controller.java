ec220a3481d72f3e3245ba4cc8e8836bb0ce84c0
fankous <119065907+fankous@users.noreply.github.com>
2023-01-27 18:06:58 +0000

    Fix Controller

--- demo/src/main/java/com/example/demo/Controller.java
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -1,8 +1,14 @@
-@RestController
-@RequestMapping(value-"/api")
-public Class Controller {
+package com.example.demo;
 
-@GetMapping(value-"/hello")
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/api")
+public class Controller {
+
+@GetMapping(value="/hello")
 public String hello() {
     return "Hello";
 }
